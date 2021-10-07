6a8dfdfb287ba68f51f19d72326e349847fc9f55
Paulo Vitor <paulo19032004@gmail.com>
2021-10-06 22:16:10 -0300

    Pre-Commit

---
+++ Controller/main.py
@@ -0,0 +1,12 @@
+# Cadastrar vendas
+
+# 1º tela para menu de cadastrar uma nova venda ou listar as que já tem
+
+# 2º tela para formulário de cadastrar uma nova venda
+
+# 3º tela listar as que já tem
+
+# Nome
+# Data
+# Preço
+# Categoria
---
+++ Model/connect.py
@@ -0,0 +1,9 @@
+# Username: S8Pm8DjSMe
+
+# Database name: S8Pm8DjSMe
+
+# Password: iLtme9GwPN
+
+# Server: remotemysql.com
+
+# Port: 3306
